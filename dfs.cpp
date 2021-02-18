a3e5e68d27727cacaab9cbe6fe35155946aa3641
ataryan4561 <ataryan4561@gmail.com>
2021-02-19 00:54:30 +0530

    fourth commit

--- dfs.cpp
+++ dfs.cpp
@@ -34,6 +34,55 @@ int connected_components(int n,vector<bool> &vis)
     }
     return count;
 }
+void single_source_dfs(vector <vector <int>> &g,int curr,vector <int> &vis,vector <int> &dis, int curr_dis)
+    {
+        vis[curr]=1;
+        dis[curr]=curr_dis;
+        for (int child : g[curr])
+        {
+            if (vis[child]==false)
+                single_source_dfs(g,child,vis,dis,curr_dis+1);
+        }
+    }
+bool bipartite(vector<vector<int>> &graph,int curr,int c,vector<int> &col,vector<bool> &vis)
+{
+    vis[curr]=1;
+    col[curr]=c;
+    for(int child: graph[curr])
+    {
+        if(!vis[child])
+        {
+            if (bipartite(graph,child,!c,col,vis)==false)
+                        return false;
+        }
+        else
+        {
+            if (col[child]==col[curr])
+                    return false;
+        }
+        return true;
+    }
+}
+bool cycle_detection(vector <vector <int>> &g, int curr, vector<bool> &vis, int par)
+    {
+        vis[curr]=true;
+        for (int child : g[curr])
+        {
+            if (vis[child]==false )       
+            {
+                if (cycle_detection(g, child,vis,curr)==true)
+                    return true;
+            }
+            else          
+            {
+                if (child !=par)     
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 // void display()
 // {
 //     loop(i,graph.size())
@@ -73,9 +122,17 @@ int main()
         // connect edges 
         }
         vector<bool> vis(n,false);
+        vector<int> vis1(n,0);
         //dfs(graph,0,vis);
-        cout<<connected_components(graph.size(),vis)<<endl;
+        // cout<<connected_components(graph.size(),vis)<<endl;
         // display(); 
         // for display of graph 
+        // vector <int> dis(graph.size(),-1);
+        // single_source_dfs(graph,1,vis1,dis,0);
+        // for (int i=0;i<graph.size();i++)
+        //     cout<<i<<"->"<<dis[i]<<endl;
+        // vector<int> col(graph.size(),-1);
+        // cout<<bipartite(graph,1,0,col,vis);
+         cout<<cycle_detection(graph,1,vis,-1);
     }
 }
